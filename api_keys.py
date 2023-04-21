b411b1d042afd1feced7f602507855e62b166240
sydneysteele03 <126841347+sydneysteele03@users.noreply.github.com>
2023-04-21 08:06:17 -0700

    WeatherPy nearly complete

--- api_keys.py
+++ api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "262c841ba48ac9aa33c90df2b5e8fbca"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "ceb74487596a4b57a2d4955da1fa815b"
